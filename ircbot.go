d59597dad40407d547e8b3f58b2ce5b6b7c39575
OgreMonk <joshua.points@gmail.com>
2013-09-28 20:14:29 -0500

    Revert "nick tracker WIP"
    This reverts commit 91415d319dea58c69fb54b82497a1909d76cb234.

--- ircbot.go
+++ ircbot.go
@@ -13,7 +13,6 @@ import (
 	"regexp"
 	"runtime"
 	"time"
-	"path/filepath"
 )
 
 var settings struct {
@@ -21,7 +20,6 @@ var settings struct {
 	Port    int
 	Channel string
 	Nick    string
-	Password string
 }
 
 var server *string
@@ -29,9 +27,6 @@ var port *int
 var nick *string
 var channel *string
 
-var currentNicks []string
-
-var changedCmds = []string{"KICK", "PART", "JOIN", "QUIT"}
 var approved = []string{"dslachut", "hummus", "OgreMonk", "acan", "patty"}
 var acts = []string{"slaps", "brofists", "chases", "examines", "flicks",
 	"hugs", "mimics", "knifes", "surveils", "pokes"}
@@ -42,7 +37,7 @@ func initialize() {
 		log.Fatal(err)
 	}
 
-	settingsFile, err := os.Open(wdir + filepath.Separator + "settings.cfg")
+	settingsFile, err := os.Open(wdir + "\\settings.cfg")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -76,7 +71,7 @@ func handle(msg *irc.Message, client *irc.Client) {
 			log.Print(err)
 		} else if match {
 			act := acts[rand.Intn(len(acts))]
-			vic := approved[rand.Intn(len(currentNicks))]
+			vic := approved[rand.Intn(len(approved))]
 			client.Send("MODE %s +o %s", *channel, nym)
 			client.Send("PRIVMSG %s :\u0001ACTION tries to give %s ops, then %s %s \u0001",
 				*channel, nym, act, vic)
@@ -89,22 +84,6 @@ func handle(msg *irc.Message, client *irc.Client) {
 			client.Send("PRIVMSG %s :No ops for you, %s!", *channel, nym)
 		}
 	}
-}
-
-//Returns list of nicks in channel excluding any nicks with "bot"
-//Could tweak to check against slice of bots in case of non-bot nick containing "bot"
-func updateNicks(msg *irc.Message) []string {
-	nickArray []string := strings.Fields(string(msg.Trailing))
-	outNicks := make ([]string, 0, 0)
-	for i:=0; i < len(nickArray); i++ {
-		nickArray[i] = strings.TrimPrefix(nickArray[i],"@")
-		if strings.Contains(strings.ToLower(nickArray[i]), "bot"){
-			break
-		}
-		outNicks = append(outNicks, nickArray[i])
-	}
-	
-	return outNicks
 }
 
 func main() {
@@ -132,34 +111,13 @@ func main() {
 	if err := client.Send("JOIN %s", *channel); err != nil {
 		log.Fatal(err)
 	}
-	
-	//Gets names of users in channel upon first joining
-	if err := client.Send("NAMES %s", *channel); err != nil {
-		log.Print(err)
-	}
 
 	for {
 		select {
 		case err := <-client.Error:
 			log.Fatal(err)
 		case msg := <-client.Received:
-			changed := false
-			for _,v := range changedCmds{
-				if v == string(msg.Command){
-					changed = true
-					break
-				}
-			}
-			switch {
-				case changed: {
-					client.Send("NAMES %s", *channel)
-				}
-				case string(msg.Command) == "353": {
-					currentNicks = updateNicks(msg)
-				}
-				default: go handle(msg, client)
-			}
-			
+			go handle(msg, client)
 		}
 	}
 }
